2055abcd4dfc247f4cee474e9df60ada121cbfb4
SarojaPandiyan <sarojapu0407@gmail.com>
2026-02-10 11:13:50 +0000

    Revert "Add Spring Security"
    This reverts commit f89f9d43360de1b8abee0acfbfea3b77cc8da3fa.

--- src/main/java/org/example/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
